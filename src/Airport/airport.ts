06e8c3dfbca0035823ecb3c01c4aaaf6cc668b51
try makara <makara.try@student.passerellesnumeriques.org>
2024-05-01 10:08:39 +0700

    Create Airport folder and Airport file and code on it

---
+++ src/Airport/airport.ts
@@ -0,0 +1,9 @@
+
+export class airport{
+    constructor(public airportName:string, public Code:number, public city:string){
+        this.airportName = airportName;
+        this.Code = Code;
+        this.city = city;
+    }
+
+}
