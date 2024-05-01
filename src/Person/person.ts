fc86dd709968d38ecee318bdd9a12673fba3986a
Your Name <you@example.com>
2024-05-01 10:00:39 +0700

    create a person file and code in it

---
+++ src/Person/person.ts
@@ -0,0 +1,7 @@
+export abstract class person{
+    constructor(protected firstName:string,protected lastName:string, protected phoneNumber:number){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+}
